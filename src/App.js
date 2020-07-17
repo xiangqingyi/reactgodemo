aa09522daefd9f1755eab8a8ae84faf76e49b785
qingyi_xiang <qingyi_xiang@kingdee.com>
2020-07-17 17:19:59 +0800

    [update] gojs init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as go from 'gojs';
+import {ReactDiagram} from 'gojs-react';
 
+function initDiagram() {
+  const $ = go.GraphObject.make;
+  const diagram = 
+      $(go.Diagram, {
+        "undoManager.isEnabled": true,
+        'clickCreatingTool.archetypeNodeData' : {text: 'new node', color:'lightblue'},
+        model: $(go.GraphLinksModel, {
+          linkKeyProperty: 'key'
+        })
+      });
+
+      diagram.nodeTemplate = 
+            $(go.Node, 'Auto',
+                new go.Binding('location', 'loc', go.Point.parse).makeTwoWay(go.Point.stringify),
+                $(go.Shape, 'RoundedRectangle', 
+                 { name: 'SHAPE', fill: 'white', strokeWidth: 0 },
+                 new go.Binding('fill', 'color')
+                ),
+                $(go.TextBlock, 
+                  {margin: 8, editable: true},
+                  new go.Binding('text').makeTwoWay()
+                  )
+            );
+            return diagram;
+}
+
+function handleModelChange(changes) {
+  alert('GoJS model changed');
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <ReactDiagram 
+          initDiagram={initDiagram}
+          divClassName="diagram-component"
+          nodeDataArray={[
+            { key: 0, text: 'Alpha', color: 'lightblue', loc: '0 0'},
+            { key: 1, text: 'Beta', color: 'orange', loc: '150 0' },
+            { key: 2, text: 'Gamma', color: 'lightblue', loc: '0 150'},
+            { key: 3, text: 'Delta', color: 'pink', loc: '150 150' }
+          ]}
+          linkDataArray={[
+            { key: -1, from: 0, to: 1 },
+            { key: -2, from: 0, to: 2},
+            { key: -3, from: 1, to: 1 },
+            { key: -4, from: 2, to: 3 },
+            { key: -5, from: 3, to: 0 }
+          ]}
+          onModelChange={handleModelChange}
+      
+      />
+
     </div>
   );
 }
